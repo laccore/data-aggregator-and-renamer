9ed1d2cd2f5fa99a38309564508b9145fee8226f
Alexander Stone <enderu@gmail.com>
2019-02-01 13:00:08 -0600

    refactoring out some of the utility functions

--- mscl_aggregator.py
+++ mscl_aggregator.py
@@ -3,6 +3,8 @@ from os import scandir
 import argparse
 import pandas as pd
 import xlsxwriter
+import utils
+
 
 def generate_file_list(input_dir):
   '''Comb through directories to generate list of files to combine.
@@ -41,120 +43,12 @@ def generate_file_list(input_dir):
   return file_list
 
 
-def validate_export_filename(export_filename, excel):
-  '''Ensure export extension matches flag, return corrected filename.
-
-  xlswriter won't export an Excel file unless the file extension is a 
-  valid Excel file extension (xsls, xls). This script assumes the flag 
-  indicates user intention, and will append a correct extension.
-
-  If not using the Excel flag, this ensures the filename ends in .csv.
-
-  Returns the validated/fixed export filename.
-  '''
-
-  extension = export_filename.split('.')[-1]
-
-  if excel:
-    if extension not in ['xlsx', 'xls']:
-      export_filename += '.xlsx'
-  else:
-    if extension != 'csv':
-      export_filename += '.csv'
-
-  return export_filename
-
-
-def open_and_clean_file(file_path):
-  '''Open file in pandas, perform some file cleanup, return a dataframe
-
-  Opens the text files output from the Geotek equipment software 
-  with a number of flags, then drops the first row of 'data' which is 
-  just the units field.
-
-  Row 0 of data is dropped for both files (it's the units row and will
-  be added back in later).
-
-  For .raw files, the second row is also dropped because the Geotek 
-  software for the raw files is always off by one compared to the .out
-  files.
-
-  Rows are dropped, whitespace is stripped from headers, and the index
-  is reset so data aligns later on.
-
-  Notes on files:
-  - files are tab-delimited
-  - skip the first row (just file metadata)
-  - tell pandas to treat empty fields as empty strings, not NaNs
-  - the 'latin1' encoding flag is needed to open the .raw files
-  '''
-
-  drop_rows = [0, 1] if file_path.name.endswith('.raw') else [0]
-
-  df = pd.read_csv(file_path, 
-                   delimiter='\t',
-                   skiprows=[0],
-                   na_filter=False,
-                   encoding='latin1')
-
-  df = df.drop(drop_rows)
-  df = df.rename(str.strip, axis='columns')
-  df = df.reset_index(drop=True)
-  
-  return df
-
-
-def clean_headers_add_units(dataframe, old_headers):
-  ''' Drop unwanted headers and add units row to data.
-
-  Any new columns will need to have a units row added to the list 
-  below, which is converted into a dict which is converted into 
-  a pandas dataframe which is then concatenated to the front of the 
-  combined data.
-
-  'SB DEPTH' is dropped because it is not relevant and often confusing.
-  '''
-
-  units_file = 'units.txt'
-  headers_file = 'readable_headers.txt'
-  drop_headers = ['SB DEPTH']
-
-  with open(units_file, 'r+') as f:
-    headers_units = [r.split(',') for r in f.read().splitlines()]
-    units = {item[0]: item[1] for item in headers_units}
-
-  with open(headers_file, 'r+') as f:
-    readable_headers = [r.split(',') for r in f.read().splitlines()]
-    new_headers = {item[0]: item[1] for item in readable_headers}
-
-  # Remove unwanted column headers
-  for dh in drop_headers:
-    if dh in old_headers:
-      old_headers.remove(dh)
-
-  # Display warnings if an unrecognized machine header is seen
-  for header in old_headers:
-    if header not in units:
-      print(f"WARNING: no associated units for header '{header}'.")
-    if header not in new_headers:
-      print(f"WARNING: no associated readable header for header '{header}'.")
-  
-  # Add units row
-  dataframe = pd.concat([pd.DataFrame([units]), dataframe], ignore_index=True)
-
-  # Fix headers
-  dataframe = dataframe.rename(columns=new_headers)
-  headers = [new_headers[header] for header in old_headers]
-
-  return dataframe, headers
-
-
 def aggregate_mscl_data(input_dir, out_filename, excel=False, verbose=False):
   ''' Aggregate cleaned data from different files and folders, export.
 
   '''
-
-  start_time = timeit.default_timer()
+  if verbose:
+    start_time = timeit.default_timer()
 
   file_list = generate_file_list(input_dir)
   if verbose:
@@ -163,7 +57,7 @@ def aggregate_mscl_data(input_dir, out_filename, excel=False, verbose=False):
       print(f'  {folder[0].name}')
     print()
 
-  export_filename = validate_export_filename(out_filename, excel)
+  export_filename = utils.validate_export_filename(out_filename, excel)
   if verbose and export_filename != out_filename:
     print(f"Adjusted export filename to '{export_filename}'")
 
@@ -174,9 +68,15 @@ def aggregate_mscl_data(input_dir, out_filename, excel=False, verbose=False):
   column_order = []
 
   # Start combining data
+  # Row 0 (unit row, added later) of data is dropped for both files.
+  # For .raw files, the second row is also dropped because the Geotek 
+  # software for the raw files is always off by one compared to the .out
+  # files.
+  skip_rows = [0]           # skip first row of mscl output files
+
   for d, out, raw in file_list:
-    out_df = open_and_clean_file(out)
-    raw_df = open_and_clean_file(raw)
+    out_df = utils.open_and_clean_file(out, skip_rows, [0])
+    raw_df = utils.open_and_clean_file(raw, skip_rows, [0,1])
 
     if verbose:
       print(f'Loaded files from {d.name}')
@@ -209,7 +109,8 @@ def aggregate_mscl_data(input_dir, out_filename, excel=False, verbose=False):
     print(f'All data combined ({len(combined_df)} rows).')
   
   # Drop unused headers, add units, and make headers human readable
-  combined_df, column_order = clean_headers_add_units(combined_df, column_order)
+  # 'SB DEPTH' is dropped because it is not relevant and often confusing.
+  combined_df, column_order = utils.clean_headers_add_units(combined_df, column_order, 'mscl', ['SB DEPTH'])
 
   # Export data
   print(f"Exporting combined data to '{export_filename}'", end='\r')
---
+++ utils.py
@@ -0,0 +1,100 @@
+import pandas as pd
+
+def validate_export_filename(export_filename, excel):
+  '''Ensure export extension matches flag, return corrected filename.
+
+  xlswriter won't export an Excel file unless the file extension is a 
+  valid Excel file extension (xsls, xls). This script assumes the flag 
+  indicates user intention, and will append a correct extension.
+
+  If not using the Excel flag, this ensures the filename ends in .csv.
+
+  Returns the validated/fixed export filename.
+  '''
+
+  extension = export_filename.split('.')[-1]
+
+  if excel:
+    if extension not in ['xlsx', 'xls']:
+      export_filename += '.xlsx'
+  else:
+    if extension != 'csv':
+      export_filename += '.csv'
+
+  return export_filename
+
+
+def open_and_clean_file(file_path, skip_rows, drop_rows):
+  '''Open file in pandas, perform some file cleanup, return a dataframe
+
+  Opens the text files output from the Geotek equipment software 
+  with a number of flags, then drops the first row of 'data' which is 
+  just the units field.
+
+  Rows are dropped, whitespace is stripped from headers, and the index
+  is reset so data aligns later on.
+
+  Notes on files:
+  - files are tab-delimited
+  - skip the first row (just file metadata)
+  - tell pandas to treat empty fields as empty strings, not NaNs
+  - the 'latin1' encoding flag is needed to open the .raw files
+  '''
+
+  df = pd.read_csv(file_path, 
+                   delimiter='\t',
+                   skiprows=skip_rows,
+                   na_filter=False,
+                   encoding='latin1')
+
+  df = df.drop(drop_rows)
+  df = df.rename(str.strip, axis='columns')
+  df = df.reset_index(drop=True)
+  
+  return df
+
+
+def clean_headers_add_units(dataframe, column_order, file_type, drop_headers=[]):
+  ''' Drop unwanted headers and add units row to data.
+
+  Any new columns will need to have a units row added to the list 
+  below, which is converted into a dict which is converted into 
+  a pandas dataframe which is then concatenated to the front of the 
+  combined data.
+  '''
+
+  if file_type not in ['mscl']:
+    print(f'Error: cannot process files of type {file_type}! Exiting.')
+    exit(1)
+
+  units_file = file_type + '_units.txt'
+  headers_file = file_type + '_headers.txt'
+
+  with open(units_file, 'r+') as f:
+    headers_units = [r.split(',') for r in f.read().splitlines()]
+    units = {item[0]: item[1] for item in headers_units}
+
+  with open(headers_file, 'r+') as f:
+    readable_headers = [r.split(',') for r in f.read().splitlines()]
+    new_headers = {item[0]: item[1] for item in readable_headers}
+
+  # Remove unwanted column headers
+  for dh in drop_headers:
+    if dh in column_order:
+      column_order.remove(dh)
+
+  # Display warnings if an unrecognized machine header is seen
+  for header in column_order:
+    if header not in units:
+      print(f"WARNING: no associated units for header '{header}'.")
+    if header not in new_headers:
+      print(f"WARNING: no associated readable header for header '{header}'.")
+  
+  # Add units row
+  dataframe = pd.concat([pd.DataFrame([units]), dataframe], ignore_index=True)
+
+  # Fix headers
+  dataframe = dataframe.rename(columns=new_headers)
+  column_order = [new_headers[header] for header in column_order]
+
+  return dataframe, column_order
